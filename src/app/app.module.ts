b12d060aaf82f3ab174add73df2a07578a0fbf0a
ltanh3499 <abcxyz030499@gmail.com>
2021-04-10 21:41:49 +0700

    bổ sung css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,30 +2,27 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ToastrModule } from "ngx-toastr";
-
 import { SidebarModule } from './sidebar/sidebar.module';
 import { FooterModule } from './shared/footer/footer.module';
 import { NavbarModule} from './shared/navbar/navbar.module';
 import { FixedPluginModule} from './shared/fixedplugin/fixedplugin.module';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
 import { AppRoutes } from './app.routing';
-
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { LoginComponent } from './login/login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { JwtHelperService, JWT_OPTIONS } from "@auth0/angular-jwt";
 import { NewsDetailComponent } from './pages/news-detail/news-detail.component';
 import { StudentInfoComponent } from './pages/student-info/student-info.component';
-import { FormsModule, NgModel } from "@angular/forms";
+import { FormsModule, NgModel, ReactiveFormsModule } from "@angular/forms";
 import { NewsSaveComponent } from './pages/news-save/news-save.component';
 import { CompanyComponent } from './pages/company/company.component';
 import { CompanyDetailComponent } from './pages/company-detail/company-detail.component';
 import {MatDialogModule, MAT_DIALOG_DEFAULT_OPTIONS} from '@angular/material/dialog';
 import { BrowserModule } from "@angular/platform-browser";
-import { EducationComponent } from './pages/education/education.component';
-import { DetailEducationComponent } from './pages/education/detail-education/detail-education.component';
+// import { EducationComponent } from './pages/education/education.component';
+// import { DetailEducationComponent } from './pages/education/detail-education/detail-education.component';
 import { ChangePasswordComponent } from './pages/student-info/change-password/change-password.component';
 
 @NgModule({
@@ -36,8 +33,8 @@ import { ChangePasswordComponent } from './pages/student-info/change-password/ch
     NewsDetailComponent,
     StudentInfoComponent,
     NewsSaveComponent,
-    EducationComponent,
-    DetailEducationComponent,
+    // EducationComponent,
+    // DetailEducationComponent,
     ChangePasswordComponent,
     CompanyComponent,
     CompanyDetailComponent
@@ -60,7 +57,7 @@ import { ChangePasswordComponent } from './pages/student-info/change-password/ch
     MatDialogModule
   ],
   entryComponents: [
-    DetailEducationComponent,
+    // DetailEducationComponent,
     ChangePasswordComponent
   ],
   providers: [
